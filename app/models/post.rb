0e3c024882633176455a2fb660788ab0063b3550
Ed Scully <escully82@gmail.com>
2016-05-06 14:15:39 -0500

    Made the relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ActiveRecord::Base
   validates :content,  presence: true
   validates :user_id,  presence: true
 
+  belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
   validates :name,  presence: true
-  validates :email, presence: true, uniqueness: true 
+  validates :email, presence: true, uniqueness: true
+
+  has_many :posts
 
 end
