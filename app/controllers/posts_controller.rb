ad1d0059e4e497e60dc7badfccf72dc825056f60
Ed Scully <escully82@gmail.com>
2016-05-06 00:12:51 -0500

    Added views. Can't create a blog. Getting error message, 'The action 'create' could not be found for PostsController'/

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,30 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to :posts
+    else
+      render :new
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :picture_url, :user_id)
+  end
+
 end
